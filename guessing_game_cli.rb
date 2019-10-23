3065d66c349bbe58ea87bab48279a238d519afef
Luke Waring <lukewaring@gmail.com>
2019-10-23 17:22:15 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,9 +1,15 @@
 def run_guessing_game
-  computer_guess = rand(6) + 1
-  puts "Guess a number between 1 and 6."
-  user_guess = gets.chomp
+	puts "Guess a number between 1 and 6."
+	computer_guess = rand(6) + 1
+	user_guess = gets.chomp
   
   if user_guess == computer_guess
-    print "You guessed the correct number!"
+    puts "You guessed the correct number!"
+  elsif user_guess != computer_guess
+    puts "The computer guessed #{computer_guess}."
+  elsif user_guess == "exit"
+    puts "Goodbye!"
+  else
+    puts "Invalid input"
   end
 end
