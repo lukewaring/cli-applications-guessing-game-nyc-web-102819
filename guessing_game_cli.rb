38b229cf743f61614b6e4480b1ee6575ee563179
Luke Waring <lukewaring@gmail.com>
2019-10-23 17:23:14 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
 	puts "Guess a number between 1 and 6."
-	computer_guess = rand(6) + 1
+	computer_guess = (rand(6) + 1).to_s
 	user_guess = gets.chomp
   
   if user_guess == computer_guess
